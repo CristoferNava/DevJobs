a3233342789f863fe836fe4f89683e61eb5901b8
CristoferNava <cj.carmonanava@ugto.mx>
2020-04-03 20:56:39 -0600

    Handlebars config was adde

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const routes = require('./routes/routes');
 
 const app = express();
+
+// Habilitamos handlebars como template engine
+app.engine('handlebars', 
+  exphbs({
+    defaultLayout: 'base'
+  })
+);
+app.set('view engine', 'handlebars');
 
 app.use('/', routes());
 
